6173770505f11a1aeb61412490fc74b438e675d6
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-28 23:28:45 +0100

    feat(gnl): add ft_strjoin and ft_strlcpy implementations
    Added ft_strjoin to concatenate dynamically allocated strings, simplifying the management of accumulated buffers and read content in get_next_line. This enhancement improves code clarity and modularity.
    
    Implemented ft_strlcpy to safely copy strings, ensuring null-termination of the destination buffer. This function is integral to ensuring robust and predictable string manipulation.

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 23:14:25 by alvinram          #+#    #+#             */
-/*   Updated: 2024/11/27 17:27:34 by alvinram         ###   ########.fr       */
+/*   Updated: 2024/11/28 23:27:42 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,4 +70,43 @@ char	*ft_strchr(const char *string, int character)
 		return ((char *)string);
 	}
 	return (NULL);
+}
+
+size_t	ft_strlcpy(char *destination, const char *source,
+		size_t destination_size)
+{
+	size_t	count;
+	size_t	length_source;
+
+	length_source = ft_strlen(source);
+	if (destination_size < 1)
+	{
+		return (length_source);
+	}
+	count = 0;
+	while (source[count] != '\0' && count < (destination_size - 1))
+	{
+		destination[count] = source[count];
+		count++;
+	}
+	destination[count] = '\0';
+	return (length_source);
+}
+
+char	*ft_strjoin(char const *string1, char const *string2)
+{
+	size_t	string1_length;
+	size_t	string2_length;
+	char	*join;
+
+	if (!string1 || !string2)
+		return (NULL);
+	string1_length = ft_strlen(string1);
+	string2_length = ft_strlen(string2);
+	join = (char *)malloc((string1_length + string2_length + 1) * sizeof(char));
+	if (!join)
+		return (NULL);
+	ft_strlcpy(join, string1, string1_length + 1);
+	ft_strlcpy((join + string1_length), string2, string2_length + 1);
+	return (join);
 }
