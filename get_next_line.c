956d043878df2c744497f4b9250ce08466e92795
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-12-13 23:55:50 +0100

    feat(gnl): add clean_depot function for buffer management
    Added clean_depot, a function designed to handle buffer cleanup in get_next_line. It ensures proper memory management by freeing unnecessary memory while retaining relevant data for subsequent operations. This enhancement improves efficiency and robustness in buffer handling.

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,61 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 22:03:40 by alvinram          #+#    #+#             */
-/*   Updated: 2024/12/06 23:20:04 by alvinram         ###   ########.fr       */
+/*   Updated: 2024/12/13 23:51:34 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *ft_free(char **string)
+char	*ft_free(char **string)
 {
-    free(*string);
-    *string = NULL;
-    return (NULL);
+	free(*string);
+	*string = NULL;
+	return (NULL);
 }
 
-char *new_line(char *depot)
+char	*new_line(char *depot)
 {
-    char *line;
-    char *pointer;
-    int length;
-    
-    pointer = ft_strchar(depot, '\n');
-    length = pointer - depot + 1;
-    line = ft_substr(depot, 0, length);
-    if (!line)
+	char	*line;
+	char	*pointer;
+	int		length;
+
+	pointer = ft_strchar(depot, '\n');
+	length = pointer - depot + 1;
+	line = ft_substr(depot, 0, length);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+char	*clean_depot(char *depot)
+{
+	char	*new_depot;
+	char	*pointer;
+	int		length;
+
+	pointer = ft_strchar(depot, '\n');
+	if (!pointer)
+	{
+		new_depot = NULL;
+		return (ft_free(&depot));
+	}
+	else
+		length = (pointer - depot) + 1;
+	if (!depot[length])
+		return (ft_free(&depot));
+	new_depot = ft_substr(depot, length, ft_strlen(depot) - length);
+	ft_free(&depot);
+	if (!new_depot)
+		return (NULL);
+	return (new_depot);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*depot = NULL;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-    return (line);
-}
-
-char    *get_next_line(int fd)
-{
-    
 }
