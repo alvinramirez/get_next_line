312ec711090fd9c0d110d911e80fb3235cf467fe
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-02-13 22:05:54 +0100

    feat(gnl): implement linked list for buffer management

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 22:01:16 by alvinram          #+#    #+#             */
-/*   Updated: 2024/11/20 22:02:57 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/02/13 22:01:25 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
 # include <stdlib.h>
-# include <limits.h>
-# include <string.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
 
+typedef struct s_list
+{
+    char    *content;
+    struct s_list *next;
+    t_list;
+}
+
 char *get_next_line(int fd);
+
+t_list *ft_lstnew(char *content);
+void ft_lstclear(t_list **lst);
+char *ft_strjoin_list(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
 
 # endif
